c802b7f94b156df48b509102121af5584dce0d1c
sfloresk <sfkanter@amazon.com>
2024-06-19 21:53:39 -0600

    Fix terraform example

--- anthropic/solutions/cloud-infra-ai/terraform/main.tf
+++ anthropic/solutions/cloud-infra-ai/terraform/main.tf
@@ -58,7 +58,7 @@ data "aws_ssm_parameter" "ecs_optimized_ami_linux_2023" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/al2023-ami-ecs-hvm-2023.0.20240610-kernel-6.1-x86_64"
 }
 resource "aws_instance" "ecs" {
-  ami           = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami_linux_2023.value)["image_id"]
+  ami           = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami_linux_2.value)["image_id"]
   instance_type = "t3.micro"
 
     network_interface {
